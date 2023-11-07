74f8bc413df33f0ecd29fcf6ac99e82619ddd532
Артем Артемов <artemov.job@gmail.com>
2023-11-07 12:51:30 +0300

    Added namespaces and names for page urls

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group_list.html/', views.group_posts)
+    path('', views.index, name='homepage'),
+    path('group_list.html/', views.group_posts, name='group_posts')
     # path('group/<slug:slug>/', views.group_posts),
 ]
