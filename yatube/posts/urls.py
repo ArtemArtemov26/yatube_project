b4277fd955800feef47cc8d8b8e7b621aadef66f
Артем Артемов <artemov.job@gmail.com>
2023-11-15 21:25:11 +0300

    Final version: Добавлена логика в шаблоны, создана модель Groups, настроены ссылки на группы постов

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,6 +5,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='homepage'),
-    path('group_list.html/', views.group_posts, name='group_posts')
+    path('group/<slug:slug>', views.group_posts, name='group_posts')
     # path('group/<slug:slug>/', views.group_posts),
 ]
